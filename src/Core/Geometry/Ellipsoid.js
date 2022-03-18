d8ae59f1f97ea2742f13164bc2986e688fb878da
yangqiao <690788010@qq.com>
2022-03-18 17:57:08 +0800

    新增空间直角坐标转大地坐标功能

--- src/Core/Geometry/Ellipsoid.js
+++ src/Core/Geometry/Ellipsoid.js
@@ -5,6 +5,7 @@
 import Geodetic3D from "../Geodetic3D.js";
 import Geodetic2D from "../Geodetic2D.js";
 import Vector3D from "../Vectors/Vector3D.js";
+import Trig from "../Trig.js";
 
 class Ellipsoid {
   static Wgs84 = new Ellipsoid(6378137.0, 6378137.0, 6356752.314245);
@@ -22,6 +23,10 @@ class Ellipsoid {
     }
     this._radii = new Vector3D(x, y, z);
     this._radiiSquared = new Vector3D(x * x, y * y, z * z);
+    this._radiiToTheFourth = new Vector3D(
+      this._radiiSquared.X * this._radiiSquared.X,
+      this._radiiSquared.Y * this._radiiSquared.Y,
+      this._radiiSquared.Z * this._radiiSquared.Z);
     this._oneOverRadiiSquared = new Vector3D(1.0 / (x * x), 1.0 / (y * y), 1.0 / (z * z));
   }
 
@@ -85,14 +90,85 @@ class Ellipsoid {
    */
   toGeodetic2D(positionOnEllipsoid) {
     const n = this.geodeticSurfaceNormal(positionOnEllipsoid);
-    return new Geodetic2D(Math.atan2(n.Y, n.X),
-      Math.asin(n.Z / n.Magnitude));
+    return new Geodetic2D(Trig.ToDegrees(Math.atan2(n.Y, n.X)),
+      Trig.ToDegrees(Math.asin(n.Z / n.Magnitude)));
   }
 
   /**
-   * 将一个任意点沿地心法线投影到椭球体表面，返回该投影点。
-   * @param  {Vector3D} positionOnEllipsoid
-   * @returns {Geodetic2D}
+   * 将一个任意点从空间直角坐标转换为大地坐标
+   * @param  {Vector3D} position
+   * @returns {Geodetic3D}
+   */
+  toGeodetic3D(position) {
+    const p = this.scaleToGeodeticSurface(position);
+    const h = position.subtract(p);
+    const height = Math.sign(h.dot(position)) * h.Magnitude;
+    const geodetic2d = this.toGeodetic2D(p)
+    return new Geodetic3D(geodetic2d.Longitude, geodetic2d.Latitude, height);
+  }
+
+  /**
+   * 将一个任意点（空间直角坐标）沿大地表面法线投影到椭球体表面，返回该投影点。
+   * @param  {Vector3D} position
+   * @returns {Geodetic3D}
+   */
+  scaleToGeodeticSurface(position) {
+    const beta = 1.0 / Math.sqrt(
+      (position.X * position.X) * this._oneOverRadiiSquared.X +
+      (position.Y * position.Y) * this._oneOverRadiiSquared.Y +
+      (position.Z * position.Z) * this._oneOverRadiiSquared.Z);
+    const n = new Vector3D(
+        beta * position.X * this._oneOverRadiiSquared.X,
+        beta * position.Y * this._oneOverRadiiSquared.Y,
+        beta * position.Z * this._oneOverRadiiSquared.Z).Magnitude;
+    let alpha = (1.0 - beta) * (position.Magnitude / n);
+
+    const x2 = position.X * position.X;
+    const y2 = position.Y * position.Y;
+    const z2 = position.Z * position.Z;
+
+    let da = 0.0;
+    let db = 0.0;
+    let dc = 0.0;
+
+    let s = 0.0;
+    let dSdA = 1.0;
+
+    do {
+      alpha -= (s / dSdA);
+
+      da = 1.0 + (alpha * this._oneOverRadiiSquared.X);
+      db = 1.0 + (alpha * this._oneOverRadiiSquared.Y);
+      dc = 1.0 + (alpha * this._oneOverRadiiSquared.Z);
+
+      const da2 = da * da;
+      const db2 = db * db;
+      const dc2 = dc * dc;
+
+      const da3 = da * da2;
+      const db3 = db * db2;
+      const dc3 = dc * dc2;
+
+      s = x2 / (this._radiiSquared.X * da2) +
+        y2 / (this._radiiSquared.Y * db2) +
+        z2 / (this._radiiSquared.Z * dc2) - 1.0;
+
+      dSdA = -2.0 *
+        (x2 / (this._radiiToTheFourth.X * da3) +
+        y2 / (this._radiiToTheFourth.Y * db3) +
+        z2 / (this._radiiToTheFourth.Z * dc3));
+    } while (Math.abs(s) > 1e-10);
+
+    return new Vector3D(
+      position.X / da,
+      position.Y / db,
+      position.Z / dc);
+  }
+
+  /**
+   * 将一个任意点沿地心表面法线投影到椭球体表面，返回该投影点。
+   * @param  {Vector3D} position
+   * @returns {Vector3D}
    */
   scaleToGeocentricSurface(position) {
     const beta = 1.0 / Math.sqrt(
