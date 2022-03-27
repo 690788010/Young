457031cd49199d592170a41751a48792aaf8dd1a
yangqiao <690788010@qq.com>
2022-03-27 09:35:46 +0800

    新增PrimitiveType.js

---
+++ src/Core/Geometry/PrimitiveType.js
@@ -0,0 +1,19 @@
+/**
+ * PrimitiveType对象是图元类型的枚举
+ */
+
+const PrimitiveType = {
+  Points: "Points",
+  Lines: "Lines",
+  LineLoop: "LineLoop",
+  LineStrip: "LineStrip",
+  Triangles: "Triangles",
+  // TriangleStrip,
+  // TriangleFan,
+  // LinesAdjacency,
+  // LineStripAdjacency,
+  // TrianglesAdjacency,
+  // TriangleStripAdjacency
+};
+
+export default PrimitiveType;
